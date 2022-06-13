bb865e37d806a3e5a1ef6dead7994ae7097328aa
chenchen <chenchen@ebupt.com>
2022-06-13 22:13:59 +0800

    完善说明文档

---
+++ web/src/components/dashboard/dashboard_chart_pie.vue
@@ -0,0 +1,57 @@
+<template>
+   <div :id="data.title" style="width: 600px; height: 400px" ></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+    props:{data:Object},
+    data(){
+        return{
+           
+        }
+    }
+,
+  // 基于准备好的dom，初始化echarts实例
+  mounted(){
+      //this.get_data()
+
+      this.charts_init(this.data)
+  },
+  methods: {
+    charts_init(chart_data) {
+        let myChart = echarts.init(document.getElementById(chart_data.title))
+            //let myChart = echarts.init(document.getElementById(chart_data[i]._id.$oid));
+            myChart.setOption({
+        title: {
+          text: chart_data.title,
+          left: 'center'
+        },
+        tooltip: {
+    trigger: 'item'
+  },
+        legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: '',
+      type: 'pie',
+      radius: '50%',
+      data: chart_data.data,
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+      });
+        
+    },
+  },
+};
+</script>
---
+++ web/src/components/main_page/main_page.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <el-row>
+            <el-col>71</el-col>
+            <el-col>81</el-col>
+            <el-col>91</el-col>
+            <el-col>100</el-col>
+
+        </el-row>
+
+    </div>
+
+
+
+
+
+</template>
+
+
+<script>
+import {
+    url,
+    data_query_url,
+    data_del_url,
+    data_commit_url,
+    ssh_check_url
+} from "@/data_interface/url";
+import { task_status_map } from "@/data_interface/map";
+import axios from "axios";
+export default {
+    data() {
+        return {
+            res_data: {
+                total_num_host: 2,
+                total_num_task: 1,
+                total_service: 3,
+                total_num_workflow: 2,
+                class_num: [
+                    {
+                        title: 'task',
+                        data: [
+                            { value: 1048, name: 'single' },
+                            { value: 735, name: 'interval' },
+                            { value: 580, name: 'cron' },
+                        ]
+                    },
+                    {
+                        title: 'service',
+                        data: [
+                            { value: 1048, name: 'shell' },
+                            { value: 735, name: '脚本' },
+                        ]
+                    },
+                ]
+            }
+
+        }
+    },
+    methods: {}
+
+}
+
+</script>
